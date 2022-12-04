8e673ac15de9170879ee5f1b005996dcc0b75196
PartEasY <minecraft.star96@gmail.com>
2022-12-04 21:41:46 +0100

    Update runthis.sh

--- runthis.sh
+++ runthis.sh
@@ -17,7 +17,7 @@ for line in $Lines
 do
 user=$line
 adduser $user
-usermod -aG sudo $user
+sudo usermod -aG sudo $user
 sudo mkdir -pv $user/Documents/
 sudo mkdir -pv $user/Desktop/
 sudo mkdir -pv $user/Downloads/
