61697d6ef65fb791043827e52fe34ce8d067c898
PartEasY <minecraft.star96@gmail.com>
2022-12-04 21:39:28 +0100

    Update runthis.sh

--- runthis.sh
+++ runthis.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+
+sudo mkdir -p home/Documents/
+sudo mkdir -p home/Desktop/
+sudo mkdir -p home/Downloads/
+sudo mkdir -p home/Pictures/
+sudo mkdir -p home/Videos/
+
+for i in 1 2 3 4 5
+do
+sudo mkdir folder$i
+done
+
 File="users.txt"
 Lines=$(cat $File)
 for line in $Lines
@@ -12,3 +24,22 @@ sudo mkdir -pv $user/Downloads/
 sudo mkdir -pv $user/Pictures/
 sudo mkdir -pv $user/Videos/
 done
+
+sudo apt update
+sudo apt upgrade
+
+sudo apt install ufw
+sudo apt install git-all
+sudo apt install nginx
+sudo apt install net-tools
+
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+
